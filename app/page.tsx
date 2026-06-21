021a3eeecdfb4606b0b7b527407717bd3af371d1
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-21 04:40:57 +0300

    modif infos

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import "./styles/pages/home.scss";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col flex-1 items-center justify-center  font-sans">
       {/* <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
