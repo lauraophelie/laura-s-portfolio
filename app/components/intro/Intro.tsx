984c4c77f75d23c5514348727f1bfdb04d0b9193
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-22 00:14:17 +0300

    start intro box

--- app/components/intro/Intro.tsx
+++ app/components/intro/Intro.tsx
@@ -1,6 +1,6 @@
 export default function Intro() {
     return (
-        <div className="col-span-2 row-span-2 text-center flex items-center justify-center">
+        <div className="col-span-2 row-span-2 text-center flex items-center justify-center home-page__box">
             Intro
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Intro from "./components/intro/Intro";
 import Copyright from "./components/misc/Copyright";
 import Process from "./components/misc/Process";
 import Status from "./components/misc/Status";
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center py-2 px-2 home-page">
       <main className="grid grid-cols-4 gap-2 w-full h-screen">
-        <div className="bg-blue-500 col-span-2 row-span-2 text-center flex items-center justify-center home-page__box">box 1</div>
+        <Intro />
         <div className="bg-blue-500 col-span-2 text-center flex items-center justify-center">box 2</div>
         <div className="bg-blue-500 flex items-center justify-center">box 3</div>
         <div className="bg-blue-500 flex items-center justify-center">box 4</div>
