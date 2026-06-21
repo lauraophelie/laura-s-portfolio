2863a8b675fab7b28d768dd1007d34f58b3749b8
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-22 00:51:17 +0300

    intro box

--- app/components/intro/Intro.tsx
+++ app/components/intro/Intro.tsx
@@ -1,7 +1,13 @@
+import "./intro.scss";
+
 export default function Intro() {
     return (
-        <div className="col-span-2 row-span-2 text-center flex items-center justify-center home-page__box">
-            Intro
+        <div className="col-span-2 row-span-2 flex intro home-page__box">
+            <div className="intro__welcoming flex justify-between gap-5">
+                <p className="intro__welcoming__text">Hi there☀️</p>
+                <p className="intro__welcoming__text">Welcome to my,</p>
+            </div>
+            {/* <h1 className="intro__main-title">Portfolio</h1> */}
         </div>
     )
 }
