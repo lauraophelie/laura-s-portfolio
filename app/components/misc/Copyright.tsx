893348d47e4da039025921650d7738ba24d7aa5f
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-21 21:11:12 +0300

    Copyright box

--- app/components/misc/Copyright.tsx
+++ app/components/misc/Copyright.tsx
@@ -2,8 +2,10 @@ import "./style.scss";
 
 export default function Copyright() {
     return (
-        <div className="col-span-2 flex items-center justify-center copyright">
-            Copyright
+        <div className="col-span-2 flex items-center justify-center copyright home-page__box">
+            <p className="copyright--text">
+                © Built and Designed by Laura Ophélie
+            </p>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import "./styles/pages/home.scss";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center py-2 px-2">
+    <div className="flex flex-col flex-1 items-center justify-center py-2 px-2 home-page">
       <main className="grid grid-cols-4 gap-2 w-full h-screen">
-        <div className="bg-blue-500 col-span-2 row-span-2 text-center flex items-center justify-center">box 1</div>
+        <div className="bg-blue-500 col-span-2 row-span-2 text-center flex items-center justify-center home-page__box">box 1</div>
         <div className="bg-blue-500 col-span-2 text-center flex items-center justify-center">box 2</div>
         <div className="bg-blue-500 flex items-center justify-center">box 3</div>
         <div className="bg-blue-500 flex items-center justify-center">box 4</div>
