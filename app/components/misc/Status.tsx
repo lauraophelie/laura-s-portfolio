1cb2ab9b8dfc9683a8ea8312168132c9a7d16119
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-23 18:08:21 +0300

    start responsive

--- app/components/misc/Status.tsx
+++ app/components/misc/Status.tsx
@@ -9,11 +9,11 @@ function GetCVButton() {
 export default function Status() {
     return (
         <div className="justify-center status home-page__box">
-            <p className="status__text">Currently</p>
+            {/* <p className="status__text">Currently</p>
             <p className="status__text">Open to</p>
             <p className="status__text">Work</p>
 
-            <GetCVButton />
+            <GetCVButton /> */}
         </div>
     )
 }
