22b46c57b7b47fccbcd4436c6cf762edcdd91942
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-23 04:29:19 +0300

    about me card

--- app/components/about/AboutMe.tsx
+++ app/components/about/AboutMe.tsx
@@ -1,9 +1,16 @@
 import "./about.scss";
 
+const infos = {
+    name: "Laura Ophélie RASOLONANDRASNA",
+    title: "Fullstack Developer",
+    study: "Msc Student, specialized in Big Data & AI",
+    location: "Based in Antananarivo, Madagascar"
+}
+
 export default function AboutMe() {
     return (
-        <div className="col-span-2 text-center flex items-center justify-center about-me home-page__box">
-            About me
+        <div className="col-span-2 flex about-me home-page__box">
+            I am {infos.name} / {infos.title} / {infos.study} / {infos.location}
         </div>
     )
 }
