b127a452999d3b54a1c479df024915c2fe4f91f7
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-23 04:17:33 +0300

    added new files

---
+++ app/components/about/AboutMe.tsx
@@ -0,0 +1,9 @@
+import "./about.scss";
+
+export default function AboutMe() {
+    return (
+        <div className="col-span-2 text-center flex items-center justify-center about-me home-page__box">
+            About me
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AboutMe from "./components/about/AboutMe";
 import Intro from "./components/intro/Intro";
 import Copyright from "./components/misc/Copyright";
 import Process from "./components/misc/Process";
@@ -10,7 +11,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center py-2 px-2 home-page">
       <main className="grid grid-cols-4 gap-2 w-full h-screen">
         <Intro />
-        <div className="bg-blue-500 col-span-2 text-center flex items-center justify-center">box 2</div>
+        <AboutMe />
         <div className="bg-blue-500 flex items-center justify-center">box 3</div>
         <div className="bg-blue-500 flex items-center justify-center">box 4</div>
         <Projects />
