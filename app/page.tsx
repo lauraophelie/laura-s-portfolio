6c541f9b1923efb6866d8cd9a90cf33d7df06fcc
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-23 05:13:36 +0300

    adjusted the homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,17 @@ import "./styles/pages/home.scss";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center py-2 px-2 home-page">
-      <main className="grid grid-cols-4 gap-2 w-full h-screen">
-        <Intro />
-        <AboutMe />
-        <Skills />
-        <Background />
-        <Projects />
-        <Status />
-        <Process />
-        <Copyright />
-      </main>
-    </div>
+    // <div className="flex flex-col flex-1 items-center justify-center py-2 px-2 home-page">
+    //       </div>
+    <main className="grid grid-cols-4 gap-2 w-full h-screen py-2 px-2 home-page">
+      <Intro />
+      <AboutMe />
+      <Skills />
+      <Background />
+      <Projects />
+      <Status />
+      <Process />
+      <Copyright />
+    </main>
   );
 }
