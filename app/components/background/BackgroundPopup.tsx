42d609faf304ad162f4ccbe99c3811863cd87019
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-24 19:15:48 +0300

    init files for popup

---
+++ app/components/background/BackgroundPopup.tsx
@@ -0,0 +1,9 @@
+import "./background.scss";
+
+export default function BackgroundPopup() {
+    return (
+        <div className="background__popup">
+            Background details
+        </div>
+    )
+}
