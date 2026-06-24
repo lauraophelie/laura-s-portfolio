1682054d47da8223d13669385e69536086098bf2
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-25 02:43:36 +0300

    adjusting some details

--- app/components/misc/Status.tsx
+++ app/components/misc/Status.tsx
@@ -1,8 +1,14 @@
+import "./style.scss";
+
+const cvLink = "https://drive.google.com/file/d/1S9BO-YOGNeLK1tYx9dCj3etMu5en2quh/view?usp=sharing";
+
 function GetCVButton() {
     return (
-        <button className="status--get-button">
-            See my CV
-        </button>
+        <a href={cvLink} target="_blank">
+            <button className="status--get-button">
+                See my CV
+            </button>
+        </a>
     )
 }
 
--- app/components/projects/ProjectCard.tsx
+++ app/components/projects/ProjectCard.tsx
@@ -1,27 +1,30 @@
 import "./projects.scss";
-import { ArrowRightCircle } from "@deemlol/next-icons"
+import { ArrowRightCircle, GitHub } from "@deemlol/next-icons"
 
 interface ProjectCardProps {
     title: string;
     techstack: string[] | string;
+    githubLink: string;
+    description?: string;
 }
 
-export default function ProjectCard({title, techstack} : ProjectCardProps) {
+export default function ProjectCard({title, techstack, githubLink, description} : ProjectCardProps) {
     return (
         <div className="projects__row__cards">
             <p className="projects__row__cards--title">
                 {title}
             </p>
+            <p className="projects__row__cards--description">
+                {description}
+            </p>
             <p className="projects__row__cards--stack">
                 {techstack}
             </p>
-            <ArrowRightCircle 
-                size={50} 
-                fill="true" 
-                color="#85C2FF" 
-                strokeWidth={0.5}
-                className="projects__row__cards--arrow"
-            />
+            <a href={githubLink} target="_blank" className="projects__row__cards--view">
+                <p>
+                    View Github
+                </p>
+            </a>
         </div>
     )
 }
