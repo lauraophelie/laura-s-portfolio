354fec8e6120255d922d080f97cfb03ab12d7f22
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-25 22:09:28 +0300

    adjusting another minor detail

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -33,7 +33,7 @@ const projectsData: Project[] = [
 export default function Projects() {
     return (
         <div className="col-span-2 row-span-2 flex flex-col gap-3 projects home-page__box">
-            <div className="projects__texts flex flex-row gap-3 justify-between items-end">
+            <div className="projects__texts flex flex-row gap-3 justify-between items-center">
                 <h1 className="projects__title">Selected Projects</h1>
                 <a href="https://github.com/lauraophelie/" target="_blank" className="projects__texts__link">
                     View all projects →
