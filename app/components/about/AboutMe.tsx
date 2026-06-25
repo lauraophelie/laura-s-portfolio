3992b2f202121a2808cd81f4b65ec9ee2c8c32b9
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-25 21:20:32 +0300

    new about me card

--- app/components/about/AboutMe.tsx
+++ app/components/about/AboutMe.tsx
@@ -1,17 +1,42 @@
 import "./about.scss";
 
+interface AboutMeProps {
+    label: string;
+    info: string;
+}
+
+function AboutMeInfo({ label, info } : AboutMeProps) {
+    return (
+        <div className="about-me__infos__box flex flex-row gap-15">
+            <p className="about-me__infos__box__label">
+                {label + "  ⁄   "}
+            </p>
+            <p className="about-me__infos__box__label">
+                {info}
+            </p>
+        </div>
+    )
+}
+
 const infos = {
     name: "Laura Ophélie RASOLONANDRASNA",
-    title: "Fullstack Developer",
-    study: "Msc Student, specialized in Big Data & AI",
-    location: "Based in Antananarivo, Madagascar",
+    title: "Msc Student in Big Data & AI, Fullstack Developer",
+    location: "Antananarivo, Madagascar",
     motiv: "My main motivation is to use my technical & creative skills to build meaningful projects"
 }
 
 export default function AboutMe() {
     return (
-        <div className="col-span-2 flex about-me home-page__box">
-            I am {infos.name} / {infos.title} / {infos.study} / {infos.location} / {infos.motiv}
+        <div className="col-span-2 flex flex-col about-me home-page__box">
+            <h1 className="about-me__title">
+                About me
+            </h1>
+            
+            <div className="about-me__infos">
+                <AboutMeInfo label={"Full name "} info={infos.name} />
+                <AboutMeInfo label={"Title "} info={infos.title} />
+                <AboutMeInfo label={"Location "} info={infos.location} />
+            </div>
         </div>
     )
 }
