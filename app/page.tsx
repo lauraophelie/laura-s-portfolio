9296975209bb0e9ff1b10298f01a857985b16aa5
Laura Ophélie <lauraophelie1@gmail.com>
2026-06-25 12:22:29 +0300

    add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import AboutMe from "./components/about/AboutMe";
 import Background from "./components/background/Background";
 import Intro from "./components/intro/Intro";
@@ -10,24 +11,18 @@ import "./styles/pages/home.scss";
 
 export default function Home() {
   return (
-    // <div className="flex flex-col flex-1 items-center justify-center py-2 px-2 home-page">
-    //       </div>
-    <main className="
-        grid grid-cols-4 
-        gap-2 
-        w-full h-screen 
-        py-2 px-2 
-        home-page
-      "
-    >
-      <Intro />
-      <AboutMe />
-      <Skills />
-      <Background />
-      <Projects />
-      <Status />
-      <Process />
-      <Copyright />
-    </main>
+    <>
+      <main className="grid grid-cols-4 gap-2 w-full h-screen py-2 px-2 home-page">
+        <Intro />
+        <AboutMe />
+        <Skills />
+        <Background />
+        <Projects />
+        <Status />
+        <Process />
+        <Copyright />
+      </main>
+      <Analytics />
+    </>
   );
 }
